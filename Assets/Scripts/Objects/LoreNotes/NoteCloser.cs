3e4260cb34e7d5294fe65715def9270ce1e51e64
Alejandro Bustos <sam.alejandro.bustos@gmail.com>
2025-07-06 16:58:03 -0300

    feat: added flexible Lore Note closing via screen tap or attack button

---
+++ Assets/Scripts/Objects/LoreNotes/NoteCloser.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Objects
+{
+    public class NoteCloser: MonoBehaviour
+    {
+        [SerializeField] private NoteObject note;
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            note?.CloseNote();
+        }
+        
+        private void Update()
+        {
+            if (Input.GetButtonDown("Attack"))
+            {
+                note?.CloseNote();
+            }
+        }
+    }
+}
